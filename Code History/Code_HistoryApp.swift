75217b235dab167b977b06e0ceecfd1cff667e2f
plimacarlos <plimacarlos@brq.com>
2023-02-04 15:17:56 -0300

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+////  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Patricia Carlos on 04/02/23.
+//  
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
