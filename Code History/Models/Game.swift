db9b5063d333ff4d28bd3fefa8b21a88ae01063a
plimacarlos <plimacarlos@brq.com>
2023-04-10 14:12:42 -0300

    add API computed properties on Game to know dquote> the current question dquote> correct and incorrect guesses dquote> number of total questions

--- Code History/Models/Game.swift
+++ Code History/Models/Game.swift
@@ -8,15 +8,39 @@
 import Foundation
 
 struct Game {
+    // MARK: - PROPERTIES
     private(set) var isOver = false
     
     private let questions = Question.allQuestions.shuffled()
     private(set) var currentQuestionIndex = 0
     
+    private(set) var guesses = [ Question: Int ]() // user guess for the current answer
+    
+        // MARK: - api : Application Programming Interface
+    var numberOfQuestions: Int {
+        questions.count
+    }
     var currentQuestion: Question {
         questions[ currentQuestionIndex ]
     }
-    private(set) var guesses = [ Question: Int ]()
+    var guessCount: (
+        correct: Int,
+        incorrect: Int
+    ) {
+        var count: (
+            correct: Int,
+            incorrect: Int
+        ) = ( 0, 0 )
+        
+        for ( question, guessedIndex ) in guesses {
+            if question.correctAnswerIndex == guessedIndex {
+                count.correct += 1
+            } else {
+                count.incorrect += 1
+            }
+        }
+        return count // returns a tuple containing the total number of correct guesses and the number of incorrect guesses
+    }
     
     mutating func makeGuessForCurrentQuestion( atIndex index: Int ) {
         guesses[currentQuestion] = index
@@ -28,4 +52,5 @@ struct Game {
             isOver = true
         }
     }
+
 }
