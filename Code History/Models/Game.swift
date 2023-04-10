7da4e993c9c7d1ab073e024830258eb4c48f761c
plimacarlos <plimacarlos@brq.com>
2023-04-10 13:53:31 -0300

    Updating the Current Question and Ending the Game

--- Code History/Models/Game.swift
+++ Code History/Models/Game.swift
@@ -8,6 +8,8 @@
 import Foundation
 
 struct Game {
+    private(set) var isOver = false
+    
     private let questions = Question.allQuestions.shuffled()
     private(set) var currentQuestionIndex = 0
     
@@ -18,5 +20,12 @@ struct Game {
     
     mutating func makeGuessForCurrentQuestion( atIndex index: Int ) {
         guesses[currentQuestion] = index
+    }
+    mutating func updateGameStatus() {
+        if currentQuestionIndex < questions.count - 1 {
+            currentQuestionIndex += 1
+        } else {
+            isOver = true
+        }
     }
 }
