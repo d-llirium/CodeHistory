10b2c17993421bb69fbd40481511d0ea8eccc421
plimacarlos <plimacarlos@brq.com>
2023-04-13 15:39:04 -0300

    Make the view a NavigationView Add NavigationLink with a HStack Add Colors

--- Code History/Views/GameView.swift
+++ Code History/Views/GameView.swift
@@ -9,7 +9,7 @@
  */
 import SwiftUI
 
-struct ContentView: View
+struct GameView: View
 {
     let question = Question(
         questionText: "What was the first computer bug?",
@@ -60,6 +60,6 @@ struct ContentView: View
  */
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        GameView()
     }
 }
